2357e4293ff1729ba013683f5de8bbfb89ff8569
abdullahbinfaisal <paybacktanki@gmail.com>
2025-07-06 20:03:43 +0500

    kd pipeline done

--- CLIC_dataset.py
+++ CLIC_dataset.py
@@ -14,33 +14,53 @@ from torchvision.transforms import (
         ToTensor,
 )
 
-
+# 2. Create custom Dataset class
 class CLICDataset(Dataset):
-    def __init__(self, root_dir, latent_dir, transform=None, split='train'):
+    def __init__(self, root_dir, transform=None, split='train'):
         self.root_dir = os.path.join(root_dir, split)
-        self.latent_dir = os.path.join(latent_dir, split)
         self.transform = transform
-
-        self.image_files = sorted([
-            f for f in os.listdir(self.root_dir)
-            if f.endswith(('.png', '.jpg', '.jpeg'))
-        ])
+        self.image_files = [f for f in os.listdir(self.root_dir)
+                           if f.endswith(('.png', '.jpg', '.jpeg'))]
 
     def __len__(self):
         return len(self.image_files)
 
     def __getitem__(self, idx):
-        img_filename = self.image_files[idx]
-        img_path = os.path.join(self.root_dir, img_filename)
-        latent_path = os.path.join(self.latent_dir, os.path.splitext(img_filename)[0] + ".pt")
-
+        img_path = os.path.join(self.root_dir, self.image_files[idx])
         image = Image.open(img_path).convert('RGB')
+
         if self.transform:
             image = self.transform(image)
 
-        teacher_latent = torch.load(latent_path)
+        return image
 
-        return image, teacher_latent
+
+# class CLICDataset(Dataset):
+#     def __init__(self, root_dir, latent_dir, transform=None, split='train'):
+#         self.root_dir = os.path.join(root_dir, split)
+#         self.latent_dir = os.path.join(latent_dir, split)
+#         self.transform = transform
+
+#         self.image_files = sorted([
+#             f for f in os.listdir(self.root_dir)
+#             if f.endswith(('.png', '.jpg', '.jpeg'))
+#         ])
+
+#     def __len__(self):
+#         return len(self.image_files)
+
+#     def __getitem__(self, idx):
+#         img_filename = self.image_files[idx]
+#         img_path = os.path.join(self.root_dir, img_filename)
+#         latent_path = os.path.join(self.latent_dir, os.path.splitext(img_filename)[0] + ".pt")
+
+#         image = Image.open(img_path).convert('RGB')
+#         if self.transform:
+#             image = self.transform(image)
+
+#         teacher_latent = torch.load(latent_path)
+
+#         return image, teacher_latent
 
 
 def build_trainloader(batch_size=32, img_dir="/home/iot/Desktop/IoT/datasets/CLIC/archive/val2017", latent_dir="/home/iot/Desktop/IoT/ILLM_VLO1_Train_Latents"):
@@ -61,8 +81,8 @@ def build_trainloader(batch_size=32, img_dir="/home/iot/Desktop/IoT/datasets/CLI
 
     transform = default_train_transform(224)
     
-    train_dataset = CLICDataset(root_dir=os.path.join(img_dir), latent_dir=os.path.join(latent_dir), transform=transform)
-    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True,)
+    train_dataset = CLICDataset(root_dir=os.path.join(img_dir), transform=transform)
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, pin_memory=False, num_workers=0)
 
     return train_loader
 
